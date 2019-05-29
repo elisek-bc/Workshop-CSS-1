1fa87c1a402a5fd85f187a395556246e559cbd91
elisek-bc <elisekppns@gmail.com>
2019-05-29 15:57:52 +0200

    styling header + extra tweaks discover part

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const sectionTitle = document.getElementsByClassName('section-title');
+const imgWrapper = document.getElementsByClassName('image-wrapper');
 TweenMax.from(sectionTitle,1,{x:"-100%"})
+TweenMax.from(imgWrapper,2,{
+    width:"0px",
+    delay: 1
+});
 console.log('hello');
+
+// Library: TweenMax
+/*
+TweenMax or TweenLite + .from(title,duration,{x:"-100%"})*/
+
